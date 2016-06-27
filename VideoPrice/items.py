d5a573274084585dc66b1d110c553e4f087f279f
root <root@MK-server.(none)>
2016-06-28 06:39:01 +0800

    study

--- VideoPrice/items.py
+++ VideoPrice/items.py
@@ -1,14 +1,18 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class VideopriceItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class VideobaseItem(scrapy.Item):
+    
+    country = scrapy.Field()
+    company = scrapy.Field()
+    year = scrapy.Field()
+    film_time = scrapy.Field()
+    time_to_market = scrapy.Field()
+    video_coding = scrapy.Field()
+    resolution = scrapy.Field()
+    dialogue = scrapy.Field()
+    region_code = scrapy.Field()
+    asin = scrapy.Field()
+    
+
+    
